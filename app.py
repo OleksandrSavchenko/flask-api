9fe4ea590e3207a2296fedf6db9a5232cbce50ba
osavchenkoNIX <osavchenko@n-ix.com>
2017-08-07 16:16:36 +0300

    First commit. requirements.txt file created. PostgreSQL connected to app through SQLAlchemy.

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:80989691699@localhost/flask-api'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/')
+def index():
+    return 'Hello Flask!'
+
+if __name__ == '__main__':
+    app.run()
